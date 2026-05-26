4a9347c9290901cc06c9a8e6c255a994ffb4218c
qlrxn1152 <qlrxn1152@naver.com>
2026-05-26 22:07:51 +0900

    init: create spring boot project

---
+++ src/main/java/com/daehoon/fitlog/FitLogApplication.java
@@ -0,0 +1,13 @@
+package com.daehoon.fitlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daehoon/fitlog/FitLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daehoon.fitlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
