67806104e5de7d3820633ad78139ca8e56f72602
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 14:03:25 +0900

    feat: add MemberService

--- src/main/java/com/daehoon/fitlog/member/service/MemberService.java
+++ src/main/java/com/daehoon/fitlog/member/service/MemberService.java
@@ -1,4 +1,15 @@
 package com.daehoon.fitlog.member.service;
 
+import com.daehoon.fitlog.member.domain.Member;
+
+import java.util.List;
+
 public interface MemberService {
+
+    Member save(Member member);
+
+    Member findById(Long id);
+
+    List<Member> findAll();
+
 }
--- src/main/java/com/daehoon/fitlog/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/daehoon/fitlog/member/service/impl/MemberServiceImpl.java
@@ -1,4 +1,33 @@
 package com.daehoon.fitlog.member.service.impl;
 
-public class MemberServiceImpl {
+import com.daehoon.fitlog.member.domain.Member;
+import com.daehoon.fitlog.member.repository.MemberRepository;
+import com.daehoon.fitlog.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Member save(Member member) {
+        return memberRepository.save(member);
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
 }
