6fae4c75d942affde49949459f9aee7d4cd96045
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 10:02:40 +0900

    feat: add Member domain

--- src/main/java/com/daehoon/fitlog/member/domain/Member.java
+++ src/main/java/com/daehoon/fitlog/member/domain/Member.java
@@ -1,4 +1,19 @@
 package com.daehoon.fitlog.member.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@Slf4j
 public class Member {
+
+    private Long id;
+    private String loginId;
+    private String password;
+    private Grade grade;
+
 }
--- src/main/java/com/daehoon/fitlog/member/repository/impl/MemberRepositoryImpl.java
+++ src/main/java/com/daehoon/fitlog/member/repository/impl/MemberRepositoryImpl.java
@@ -1,4 +1,28 @@
 package com.daehoon.fitlog.member.repository.impl;
 
-public class MemberRepositoryImpl {
+import com.daehoon.fitlog.member.domain.Member;
+import com.daehoon.fitlog.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Slf4j
+public class MemberRepositoryImpl implements MemberRepository {
+
+
+    @Override
+    public Member save(Member member) {
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return List.of();
+    }
 }
