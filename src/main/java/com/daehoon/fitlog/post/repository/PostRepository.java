64f3bb8db324afe9a73ec3bb3afb3062ab837583
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 17:35:25 +0900

    feat: add PostRepository

---
+++ src/main/java/com/daehoon/fitlog/post/repository/PostRepository.java
@@ -0,0 +1,4 @@
+package com.daehoon.fitlog.post.repository;
+
+public interface PostRepository {
+}
---
+++ src/main/java/com/daehoon/fitlog/post/repository/impl/PostRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.daehoon.fitlog.post.repository.impl;
+
+import com.daehoon.fitlog.post.repository.PostRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+public class PostRepositoryImpl implements PostRepository {
+}
