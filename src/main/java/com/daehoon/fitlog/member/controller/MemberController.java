8aa708956baea4b6085001ff39d6b97028bd1617
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 17:30:48 +0900

    docs: delete almost files

--- src/main/java/com/daehoon/fitlog/member/controller/MemberController.java
+++
@@ -1,9 +0,0 @@
-package com.daehoon.fitlog.member.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MemberController {
-
-
-}
--- src/main/java/com/daehoon/fitlog/member/domain/Grade.java
+++
@@ -1,6 +0,0 @@
-package com.daehoon.fitlog.member.domain;
-
-public enum Grade {
-
-    ADMIN, GUEST, USER
-}
