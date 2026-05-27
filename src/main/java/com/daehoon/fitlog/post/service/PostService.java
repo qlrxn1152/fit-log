af73caaabc63a2ac8f5c04339fb1e7c6659173b2
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 18:02:09 +0900

    feat: add Postservice

--- src/main/java/com/daehoon/fitlog/post/service/PostService.java
+++ src/main/java/com/daehoon/fitlog/post/service/PostService.java
@@ -1,4 +1,24 @@
 package com.daehoon.fitlog.post.service;
 
+import com.daehoon.fitlog.post.domain.Post;
+
+import java.util.List;
+
 public interface PostService {
+
+    Post save(Post post);
+
+    Post findById(Long id);
+
+    List<Post> findAll();
+
+    Post findByTitle(String title);
+
+    Post editTitle(Post existPost, String editTitle);
+
+    Post editContent(Post existPost, String editContent);
+
+    void delete(Post post);
+
+    void clearStore();
 }
--- src/main/java/com/daehoon/fitlog/post/service/impl/PostServiceImpl.java
+++ src/main/java/com/daehoon/fitlog/post/service/impl/PostServiceImpl.java
@@ -1,12 +1,59 @@
 package com.daehoon.fitlog.post.service.impl;
 
+import com.daehoon.fitlog.post.domain.Post;
+import com.daehoon.fitlog.post.repository.PostRepository;
 import com.daehoon.fitlog.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
 
+    // TODO : 여러 검증로직들 추가해줘야함
+    private final PostRepository postRepository;
 
+    @Override
+    public Post save(Post post) {
+        return postRepository.save(post);
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return postRepository.findById(id);
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post findByTitle(String title) {
+        return postRepository.findByTitle(title);
+    }
+
+    @Override
+    public Post editTitle(Post existPost, String editTitle) {
+        return postRepository.editTitle(existPost, editTitle);
+    }
+
+    @Override
+    public Post editContent(Post existPost, String editContent) {
+        return postRepository.editContent(existPost, editContent);
+    }
+
+    @Override
+    public void delete(Post post) {
+        postRepository.delete(post);
+    }
+
+    @Override
+    public void clearStore() {
+        postRepository.clearStore();
+    }
 }
