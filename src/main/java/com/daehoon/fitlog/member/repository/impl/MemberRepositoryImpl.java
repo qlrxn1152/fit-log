b1e056485e2a38c86f8f9bbfe4ad2b87e85c9342
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 10:07:06 +0900

    feat: add MemberRepositoryImpl

--- src/main/java/com/daehoon/fitlog/member/repository/impl/MemberRepositoryImpl.java
+++ src/main/java/com/daehoon/fitlog/member/repository/impl/MemberRepositoryImpl.java
@@ -5,25 +5,38 @@ import com.daehoon.fitlog.member.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @Slf4j
 public class MemberRepositoryImpl implements MemberRepository {
 
+    // TODO : 동시성 문제 가능성있음 => AtomicLong, ConcurrentHashMap 으로 변경
+    private Map<Long, Member> store = new HashMap<>();
+
 
     @Override
     public Member save(Member member) {
-        member.getId();
+        log.info("[MemberRepositoryImpl] [SAVE] member: {}", member);
+        Long id = member.getId();
+        store.put(++id, member);
+
+        return member;
     }
 
     @Override
     public Member findById(Long id) {
-        return null;
+        log.info("[MemberRepositoryImpl] [FindById] memberId: {}", id);
+        return store.get(id);
     }
 
     @Override
     public List<Member> findAll() {
-        return List.of();
+        log.info("[MemberRepositoryImpl] [FindAll]");
+
+        return new ArrayList<>(store.values());
     }
 }
