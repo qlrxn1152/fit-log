45d1b7fea0750fec3dce35a8b115a8ead46cc01b
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 10:42:26 +0900

    feat: add MemberRepository TestCode

--- src/test/java/com/daehoon/fitlog/member/repository/impl/MemberRepositoryImplTest.java
+++ src/test/java/com/daehoon/fitlog/member/repository/impl/MemberRepositoryImplTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class MemberRepositoryImplTest {
 
     private final MemberRepository memberRepository = new MemberRepositoryImpl();
