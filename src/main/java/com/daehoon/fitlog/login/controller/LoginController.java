489228269bbc0557af1cf162e2ad6962bd07a7f7
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 21:23:36 +0900

    feat: add member files

---
+++ src/main/java/com/daehoon/fitlog/login/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.daehoon.fitlog.login.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class LoginController {
+}
---
+++ src/main/java/com/daehoon/fitlog/member/domain/Member.java
@@ -0,0 +1,16 @@
+package com.daehoon.fitlog.member.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter @ToString @EqualsAndHashCode
+public class Member {
+
+    private Long id;
+    private String username;
+    private String password;
+
+}
