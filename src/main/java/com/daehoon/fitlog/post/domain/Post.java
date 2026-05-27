2c94d24304bde77a5cc7ad83a0b22eec61557d05
qlrxn1152 <qlrxn1152@naver.com>
2026-05-27 17:34:37 +0900

    feat: add post domain

--- src/main/java/com/daehoon/fitlog/post/domain/Post.java
+++ src/main/java/com/daehoon/fitlog/post/domain/Post.java
@@ -1,4 +1,26 @@
 package com.daehoon.fitlog.post.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter @ToString @EqualsAndHashCode
 public class Post {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
 }
